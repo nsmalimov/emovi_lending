6d41fa49df1f9891c52db490d7ab2b07b95835a0
Nurislam Alimov <nalimov@sardinasystems.com>
2019-02-08 22:58:57 +0300

    основной каркас

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from './top/top.component';
-import { BottomComponent } from './bottom/bottom.component';
+import { ButtomComponent } from './buttom/buttom.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopComponent,
-    BottomComponent
+    ButtomComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent, TopComponent]
+  bootstrap: [AppComponent, TopComponent, SecondComponent, ButtomComponent]
 })
 export class AppModule { }
